2ff14d392ae7a08db0a181a163de11b4e2607152
dragonvirgo <norihide.shimatani@gmail.com>
2012-06-03 17:48:06 +0900

    削った。でも途中
    Signed-off-by: dragonvirgo <norihide.shimatani@gmail.com>

--- Eraser/src/shima/android/PaintView.java
+++ Eraser/src/shima/android/PaintView.java
@@ -2,102 +2,61 @@ package shima.android;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class PaintView extends View {
+	private static final float TOLERANCE = 6;
+	
+	Xfermodes xfermodes;
+	private Bitmap bmp;
+	Canvas pcanvas;
+	Path path;
+	private Paint bmpPaint;
+	private PointF prev = new PointF();
 
-		/**
-		 * 
-		 */
-		Xfermodes xfermodes;
-		private Bitmap bmp;
-		Canvas pcanvas;
-		Path path;
-		private Paint bmpPaint;
-		private float mX, mY;
-		private static final float TOUCH_TOLERANCE = 4;
-
-		public PaintView(Context context) { this(context, null); }
-		public PaintView(Context context, AttributeSet attrs) {
-			super(context, attrs);
-			this.setBackgroundColor(Color.alpha(0));
-			this.path = new Path();
-			this.bmpPaint = new Paint();
-		}
-		
-		@Override
-		protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-			super.onSizeChanged(w, h, oldw, oldh);
-			this.bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-//			Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.image);
-//			if (!b.isMutable()) {
-//				    this.bmp = b.copy(Bitmap.Config.ARGB_8888, true);
-//			}
-			this.pcanvas = new Canvas(this.bmp);
-		}
-
-		@Override
-		protected void onDraw(Canvas canvas) {
-//			canvas.drawColor(Color.WHITE);
-//			canvas.drawColor(Color.BLUE);
-			canvas.drawColor(Color.alpha(0));
-			canvas.drawBitmap(this.bmp, 0, 0, this.bmpPaint);
-//			canvas.drawPath(this.path, paint);
-			pcanvas.drawPath(this.path, this.xfermodes.paint);
-		}
-
-		private void touch_start(float x, float y) {
-			this.path.reset();
-			this.path.moveTo(x, y);
-			this.mX = x;
-			this.mY = y;
-		}
-
-		private void touch_move(float x, float y) {
-			float dx = Math.abs(x - this.mX);
-			float dy = Math.abs(y - this.mY);
-			if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
-				// draws a quadratic curve
-				this.path.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
-				mX = x;
-				mY = y;
-			}
-		}
-
-		private void touch_up() {
-			this.path.lineTo(mX, mY);
-			// commit the path to our offscreen
-			this.pcanvas.drawPath(this.path, this.xfermodes.paint);
-			// kill this so we don't double draw
-			this.path.reset();
-		}
-
-		@Override
-		public boolean onTouchEvent(MotionEvent e) {
-			float x = e.getX();
-			float y = e.getY();
-
-			switch (e.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-				touch_start(x, y);
-				invalidate();
-				break;
-			case MotionEvent.ACTION_MOVE:
-				touch_move(x, y);
-				invalidate();
-				break;
-			case MotionEvent.ACTION_UP:
-				touch_up();
-				invalidate();
-				break;
-			}
-			return true;
-		}
+	public PaintView(Context context) { this(context, null); }
+	public PaintView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setBackgroundColor(Color.alpha(0));
+		path = new Path();
+		bmpPaint = new Paint();
 	}
+	@Override protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		pcanvas = new Canvas(bmp);
+	}
+	@Override protected void onDraw(Canvas canvas) {
+		canvas.drawColor(Color.alpha(0));
+		canvas.drawBitmap(bmp, 0, 0, bmpPaint);
+		pcanvas.drawPath(path, xfermodes.paint);
+	}
+	@Override public boolean onTouchEvent(MotionEvent e) {
+		float x = e.getX(); float y = e.getY();
+		switch (e.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			path.reset();
+			path.moveTo(x, y);
+			break;
+		case MotionEvent.ACTION_MOVE:
+			if (Math.abs(x - prev.x) >= TOLERANCE || Math.abs(y - prev.y) >= TOLERANCE) {
+				path.quadTo(prev.x, prev.y, (prev.x + x) / 2, (prev.y + y) / 2);
+			}
+			break;
+		case MotionEvent.ACTION_UP:
+			path.lineTo(x, y);
+			pcanvas.drawPath(path, xfermodes.paint);
+			break;
+		}
+		prev.x = x; prev.y = y;
+		invalidate();
+		return true;
+	}
+}
--- Eraser/src/shima/android/Xfermodes.java
+++ Eraser/src/shima/android/Xfermodes.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2007 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package shima.android;
 
 import android.app.Activity;
@@ -34,27 +18,23 @@ import android.widget.Toast;
 public class Xfermodes extends Activity {
 
 	public Paint paint;
-	public PaintView pv;
-	public float stroke = 4;
-	public int colour = Color.RED;
+	public PaintView paintView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		this.pv = new PaintView(getApplicationContext());
-//		setContentView(pv);
 		setContentView(R.layout.main);
-		PaintView paintView = (PaintView)findViewById(R.id.paintView);
+		paintView = (PaintView)findViewById(R.id.paintView);
 		paintView.xfermodes = this;
 
-		this.paint = new Paint();
-		this.paint.setAntiAlias(true);
-		this.paint.setDither(true);
-		this.paint.setColor(this.colour);
-		this.paint.setStyle(Paint.Style.STROKE);
-		this.paint.setStrokeJoin(Paint.Join.ROUND);
-		this.paint.setStrokeCap(Paint.Cap.ROUND);
-		this.paint.setStrokeWidth(this.stroke);
+		paint = new Paint();
+		paint.setAntiAlias(true);
+		paint.setDither(true);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeJoin(Paint.Join.ROUND);
+		paint.setStrokeCap(Paint.Cap.ROUND);
+		paint.setStrokeWidth(20);
+		paint.setColor(Color.RED);
 	}
 
 	@Override
@@ -67,77 +47,19 @@ public class Xfermodes extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.pencil:
-			// erasing disabled
 			paint.setXfermode(null);
 			paint.setAlpha(255);
-			return true;
-
+			break;
 		case R.id.eraser:
-			// erasing enabled
 			paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-//			paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
 			paint.setAlpha(0);
-			
-			return true;
-
-		case R.id.stroke:
-			this.strokeDialog();
-			return true;
-
-		case R.id.colour:
-			this.colourPicker();
-			return true;
-
+			break;
 		case R.id.clear:
-			this.pv.path = new Path(); //empty path
-			this.pv.pcanvas.drawColor(Color.WHITE);
-			this.pv.invalidate();
-			return true;
+			paintView.path = new Path(); //empty path
+			paintView.pcanvas.drawColor(Color.WHITE);
+			paintView.invalidate();
+			break;
 		}
 		return true;
 	}
-
-	public void strokeDialog() {
-		AlertDialog.Builder alert = new AlertDialog.Builder(this);
-
-		alert.setTitle("Stroke");
-		alert.setMessage("New stroke size");
-		final EditText input = new EditText(this);
-		input.setText(String.valueOf(stroke));
-		alert.setView(input);
-
-		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				String value = input.getText().toString();
-				try {
-					// tmp is used in case an exception is raised
-					float tmp = Float.valueOf(value);
-					stroke = tmp;
-					paint.setStrokeWidth(stroke);
-				} catch (Exception e) {
-					Toast.makeText(getApplicationContext(),
-							"Invalid stroke value", Toast.LENGTH_SHORT).show();
-				}
-			}
-		});
-
-		alert.setNegativeButton("Cancel",
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int whichButton) {
-						// Canceled.
-					}
-				});
-
-		alert.show();
-	}
-
-	/*
-	 * Thanks to these guys http://code.google.com/p/android-color-picker/
-	 */
-	public void colourPicker() {
-	}
-
-	/*
-	 * This class holds the drawing surface
-	 */
 }
